956859ef52dc8090c9b178709d74d713a6fccf32
thelostsoul5 <1095533751@qq.com>
2019-02-20 08:54:02 +0000

    second

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App";
-import Antd from "ant-design-vue/es";
-import "ant-design-vue/dist/antd.css";
+import 'ant-design-vue/dist/antd.css';
+import Antd from 'ant-design-vue/es';
+import Vue from 'vue';
+import App from './App';
+
 Vue.use(Antd);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
+  el: '#app',
   components: { App },
-  template: "<App/>"
+  template: '<App/>',
 });
